cdd444a7c3067bf1b9ddd0d7784c8be97d47d955
gokulkrishh <krishnangokul9@gmail.com>
2019-02-07 22:45:26 +0530

    Some changes for UI

--- src/Habits/index.js
+++ src/Habits/index.js
@@ -22,15 +22,21 @@ const Habits = props => {
       {props.habits.map(habit => {
         return (
           <div className="card" key={habit.id}>
-            <label className="card__checkbox">
-              <input type="checkbox" checked={habit.done} />
-              <span>
-                <img src={tickSVG} alt="done" />
-              </span>
-            </label>
-            <div className="card__info">
-              <h3 className="name">{habit.name}</h3>
-              <span className="time">{habit.time}</span>
+            <div className="card__left">
+              <label className="card__checkbox">
+                <input type="checkbox" checked={habit.done} />
+                <span>
+                  <img src={tickSVG} alt="done" />
+                </span>
+              </label>
+
+              <div className="card__info">
+                <h3 className="name">{habit.name}</h3>
+                <span className="time">{habit.time}</span>
+              </div>
+            </div>
+            <div className="card__right">
+              <span className="steaks">2 in a row</span>
             </div>
           </div>
         );
